794636ace9c677b14773a34d37f41a3ce0dda60d
Julia Astakhova <jastakhova@griddynamics.com>
2013-05-13 22:28:24 -0700

    no explicit phases, recipe pipeline, provisioning cluster at its declaration

--- src/main/scala/Lib.scala
+++ src/main/scala/Lib.scala
@@ -5,13 +5,13 @@ package main.scala
  * Date: 5/7/13
  */
 // Input parameters that are configured before launching manifest
-case class Input(description: String, defaultValue: String)(implicit application: Application) {
+case class Input(description: String, defaultValue: String)(implicit context: Context) {
   def value: String = ""
 }
 // Output parameters that will be available on the instance page after manifest was successfully executed
-case class Output(name: String, description: String, value: String)(implicit application: Application)
+case class Output(name: String, description: String, value: String)(implicit context: Context)
 // Environment Parameters that are specified in environment or cloud provider
-case class EnvParam(name: String)(implicit application: Application) {
+case class EnvParam(name: String)(implicit context: Context) {
   def value: String = ""
 }
 
@@ -22,8 +22,7 @@ class Promise[T] {
 }
 
 // Phase of the execution that can return some result
-case class PhaseWithResult[R](override val name: String)(steps: Context => Unit)(implicit context: Context)
-  extends Phase(name: String)(steps: Context => Unit) {
+case class PhaseWithResult[R](name: String)(steps: Context => Unit)(implicit context: Context){
   def result: Promise[R] = new Promise[R]()
 }
 
@@ -32,25 +31,14 @@ class ProvisionVmsOutput{
   def ips: List[String] = List()
 }
 
-// Node or some number of the nodes configured similarly
-case class Cluster(name: String, quantity: Int = 1)(implicit application: Application) {
+case class Cluster(clusterName1: String, identity1: String, credential1: String, quantity1: Int = 1)(implicit context: Context)
+  extends ClusterPhase[ProvisionVmsOutput](clusterName1, identity1, credential1, quantity1)("provisionVms")(context)
 
-  // Execution of the recipe that acts as a phase
-  case class Recipe(recipeName: String)(steps: Context => Unit)(implicit context: Context) extends Phase(name: String)(steps: Context => Unit) {
-    def :<(params: Map[String, Any]): Recipe = this
-  }
+// Node or some number of the nodes configured similarly that are provisioned
+class ClusterPhase[R](clusterName: String, identity: String, credential: String, quantity: Int = 1)(phaseName: String)(implicit context: Context)
+  extends PhaseWithResult[R](phaseName)({_ =>})(context) {
 
-  // Node can be asked to be provisioned. Configuration for provisioning is provided as parameters
-  def provisionVms(identity: String, credential: String)(implicit context: Context): PhaseWithResult[ProvisionVmsOutput] = {
-    val doNothing: Context => Unit  = {_ =>}
-    new PhaseWithResult[ProvisionVmsOutput]("provisionVms")(doNothing)(context)
-  }
-
-  // Node can be asked to run chef recipe on it
-  def recipe(name: String, cookbooksURL: String)(implicit context: Context): Recipe = {
-    val doNothing: Context => Unit  = {_ =>}
-    new Recipe(name)(doNothing)(context)
-  }
+  def recipe(recipeName: String, cookbooksURL: String, params: Map[String, Any] = Map())(implicit context: Context): ClusterPhase[R] = this
 }
 
 // Context is used to bind phase nesting and order (inside phases and the command)
@@ -62,15 +50,20 @@ case class Phase(name: String)(steps: Context => Unit)(implicit context: Context
   // Parts of the deployment can be bound in terms of dependecy.
   // This method allows to point which phases the current one should wait for
   def waits(phases: Phase*): Phase = this
+
+  // returns the subsequent phase
+  def ->(subsequent: Phase): Phase = this
 }
 
-// Upper-level command that will be available to be run on the instance from the ui intance page
-case class Command(name: String)(steps: Context => Unit)(implicit application: Application)
+case class Workflow(name: String)(steps: Context => Unit)
+
+case class DestroyWorkflow(steps: Context => Unit) extends Workflow("destroy")(steps)
 
 // Manifest application in total
-class Application {
+object Application {
 
-  // Build and output to STDOUT string yml manifest
-  def build { println("<builded manifest>") }
+  // Builds and outputs to STDOUT string yml manifest
+  // Has workflows as parameters. Ensures that there is at least launch workflow
+  def build(launch: Context => Unit, workflows: Workflow*) { println("<builded manifest>") }
 }
 
--- src/main/scala/Petclinic.scala
+++ src/main/scala/Petclinic.scala
@@ -2,72 +2,69 @@ import main.scala._
 import scala.collection.immutable._
 
 object Petclinic {
-  implicit val application = new Application()
 
-  val appFork = Input("Source control fork", "dieu")
-  val appBranch = Input("Source control branch", "HEAD")
+  def main(args: Array[String]) {
+    Application.build({ implicit context =>
 
-  val db = Cluster("db")
-  val app = Cluster("app", 2)
-  val lb = Cluster("lb")
+      val appFork = Input("Source control fork", "dieu")
+      val appBranch = Input("Source control branch", "HEAD")
 
-  Command("launch") { implicit context =>
+      val identity = EnvParam("identity").value
+      val credential = EnvParam("credential").value
 
-    val node2provision = Seq(db, app, lb).foldLeft(Map[Cluster, (PhaseWithResult[ProvisionVmsOutput], Promise[List[String]])]())
-      { case (m, node) => {
-        val provision = node.provisionVms(EnvParam("identity").value, EnvParam("credential").value)
-        m.updated(node, (provision, provision.result.map(_.ips)))
-      }
-    }
+      val db = Cluster("db", identity, credential)
+      val app = Cluster("app", identity, credential, 2)
+      val lb = Cluster("lb", identity, credential)
 
-    Phase("db-tune") { implicit context: Context =>
-      val installDB = db.recipe("mysql::server", "http://cookbooks") :<
-          Map("mysql" ->
-            Map(
-               "server_root_password" -> "…",
-               "server_repl_password" -> "…",
-               "server_debian_password" -> "..."))
-      (db.recipe("war::mysql", "http://cookbooks") :<
-          Map("database" ->
-            Map(
-              "name" -> "petclinic",
-              "schema" -> "…",
-              "data" -> "..."))).waits(installDB)
-    }.waits(node2provision(db)._1)
+      val cookbookPath = "http://cookbooks"
 
-    val installLB = lb.recipe("haproxy", "http://cookbooks").waits(node2provision(lb)._1)
+      db.recipe("mysql::server", cookbookPath,
+        Map("mysql" ->
+          Map(
+            "server_root_password" -> "…",
+            "server_repl_password" -> "…",
+            "server_debian_password" -> "..."))
+      ).recipe("war::mysql", cookbookPath,
+        Map("database" ->
+          Map(
+            "name" -> "petclinic",
+            "schema" -> "…",
+            "data" -> "..."))
+      )
 
-    Phase("app-tune") { implicit context: Context =>
-      val installApp = app.recipe("tomcat", "http://cookbooks")
-      val deployApp = Phase("deploy-app") { implicit context: Context =>
-        val deployApp = app.recipe("war", "http://") :<
-          Map("war" -> Map("deploy" -> Map("git" -> Map(
-            "url" -> "git",
-            "revision" -> appBranch.value
-          ))))
-        (app.recipe("war::configure", "http://") :<
-          Map("configure" -> Map(
-            "source" -> "https://",
-            "to" -> "https://",
-            "variables" ->
-              Map("hosts" -> node2provision(db)._2.value.mkString(","),
-                  "database" -> "petclinic")
-          ))
-        ).waits(deployApp)
-      }.waits(installApp)
+      val lbWithHaproxy = lb.recipe("haproxy", cookbookPath)
 
-      (lb.recipe("war::lb", "http://cookbooks")
-        :< Map("haproxy.rebalance" -> Map("nodes" -> node2provision(app)._2.value.mkString(",")))
-        ).waits(deployApp)
-    }.waits(installLB)
+      var appWithTomcat = app.recipe("tomcat", cookbookPath)
 
-    Output("db-hosts", "Database IP address", node2provision(db)._2.value.mkString(","))
-    Output("app-hosts", "Application IP address", node2provision(app)._2.value.mkString(","))
-    Output("lb-hosts", "Load Balancer IP address", node2provision(lb)._2.value.mkString(","))
-    Output("app-url", "Url to the application", "http://" + node2provision(lb)._2.value.head)
-    Output("haproxy-url", "Url to haproxy stats", "http://" + node2provision(lb)._2.value.head + ":22002/")
+      lbWithHaproxy -> appWithTomcat
+
+      val appConfigured = appWithTomcat.recipe("war", "http://",
+        Map("war" -> Map("deploy" -> Map("git" -> Map(
+          "url" -> "git",
+          "revision" -> appBranch.value
+        ))))
+      ).recipe("war::configure", "http://",
+        Map("configure" -> Map(
+          "source" -> "https://",
+          "to" -> "https://",
+          "variables" ->
+            Map("hosts" -> db.result.value.ips.mkString(","),
+              "database" -> "petclinic")
+        ))
+      )
+
+      val haproxyRebalanced = lbWithHaproxy.recipe("war::lb", "http://cookbooks",
+        Map("haproxy.rebalance" -> Map("nodes" -> app.result.value.ips.mkString(",")))
+      )
+
+      appConfigured -> haproxyRebalanced
+
+      Output("db-hosts", "Database IP address", db.result.value.ips.mkString(","))
+      Output("app-hosts", "Application IP address", app.result.value.ips.mkString(","))
+      Output("lb-hosts", "Load Balancer IP address", lb.result.value.ips.mkString(","))
+      Output("app-url", "Url to the application", "http://" + lb.result.value.ips.head)
+      Output("haproxy-url", "Url to haproxy stats", "http://" + lb.result.value.ips.head + ":22002/")
+    })
   }
-
-  application.build
 }
 
